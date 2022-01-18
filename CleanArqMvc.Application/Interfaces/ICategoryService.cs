88e972ac2b8a2d71a982dbfd37bd613b7a126c55
João Victor Velho <joaovictor.rv39@gmail.com>
2022-01-17 21:25:36 -0300

    application dtos and services

---
+++ CleanArqMvc.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using CleanArqMvc.Application.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArqMvc.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDTO>> GetCategories();
+        Task<CategoryDTO> GetById(int? id);
+        Task Add(CategoryDTO categoryDTO);
+        Task Update(CategoryDTO categoryDTO);
+        Task Remove(int? id);
+    }
+}
