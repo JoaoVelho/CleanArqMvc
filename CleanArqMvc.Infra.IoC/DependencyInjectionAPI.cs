629763497bbbf1777625beafa037d6d314e6c6f8
João Victor Velho <joaovictor.rv39@gmail.com>
2022-02-22 19:39:19 -0300

    feat: add api and jwt auth

---
+++ CleanArqMvc.Infra.IoC/DependencyInjectionAPI.cs
@@ -0,0 +1,45 @@
+﻿using CleanArqMvc.Application.Interfaces;
+using CleanArqMvc.Application.Mappings;
+using CleanArqMvc.Application.Services;
+using CleanArqMvc.Domain.Account;
+using CleanArqMvc.Domain.Interfaces;
+using CleanArqMvc.Infra.Data.Context;
+using CleanArqMvc.Infra.Data.Identity;
+using CleanArqMvc.Infra.Data.Repositories;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+namespace CleanArqMvc.Infra.IoC
+{
+    public static class DependencyInjectionAPI
+    {
+        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"
+            ), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            var myhandlers = AppDomain.CurrentDomain.Load("CleanArqMvc.Application");
+            services.AddMediatR(myhandlers);
+
+            return services;
+        }
+    }
+}
